175968544561e71129f7a7f4e9f1de8f5e03854b
Fafou40 <sousleys.fabien@orange.fr>
2023-10-24 14:37:31 +0200

    wip modif collapse

--- src/components/Layout/Collapse.jsx
+++ src/components/Layout/Collapse.jsx
@@ -1,29 +1,41 @@
-import React from 'react';
-import { useState } from "react";
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
-
-const angleDown = <FontAwesomeIcon icon={faAngleDown} />
-const angleUp = <FontAwesomeIcon icon={faAngleUp} />
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 
 const Collapse = (props) => {
-    const [ openTab, setOpenTab ] = useState(false);
+  const [openTab, setOpenTab] = useState(false);
+  const [iconRotation, setIconRotation] = useState(0);
 
-    const openTabHandler = () => {
-        setOpenTab((openTab) => !openTab); 
-    };
+  const openTabHandler = () => {
+    setOpenTab((prevOpenTab) => !prevOpenTab);
 
-    return (
-        <>
-            <div onClick={openTabHandler} className="collapse_header">
-                <h2 className="collapse_header_title">{props.title}</h2>
-                {!openTab ? <i className="collapse_header_icon">{angleDown}</i> : <i className="collapse_header_icon">{angleUp}</i>}
-            </div>
-            {openTab && 
-                <div className="collapse_content">{props.content}</div>
-            }
-        </>
-    )
-}
+    if (iconRotation === 0) {
+      // Rotation vers la gauche (-90 degrés) avant la rotation complète (-180 degrés)
+      setIconRotation(-90);
+      setTimeout(() => setIconRotation(-180), 150); // Définissez une pause pour l'étape intermédiaire
+    } else if (iconRotation === -180) {
+      // Rotation vers la droite (90 degrés) avant de revenir à la position initiale (0 degrés)
+      setIconRotation(90);
+      setTimeout(() => setIconRotation(0), 150); // Définissez une pause pour l'étape intermédiaire
+    } else {
+      setIconRotation(0); // Réinitialisation de la rotation à 0 degrés (vers le bas)
+    }
+  };
+
+  return (
+    <>
+      <div onClick={openTabHandler} className="collapse_header">
+        <h2 className="collapse_header_title">{props.title}</h2>
+        <i
+          className={`collapse_header_icon icon-rotate`}
+          style={{ transform: `rotate(${iconRotation}deg)` }}
+        >
+          <FontAwesomeIcon icon={faAngleUp} />
+        </i>
+      </div>
+      {openTab && <div className="collapse_content">{props.content}</div>}
+    </>
+  );
+};
 
 export default Collapse;
